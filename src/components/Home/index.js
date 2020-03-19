baff9451461030e9f942c7d247d205bd207c8e71
SP <sp.ukr.rs@gmail.com>
2020-03-19 13:37:22 +0200

    up to router

---
+++ src/components/Home/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import agent from '../../agent';
+
+import MainView from './MainView';
+import Banner from './Banner';
+
+const Promise = global.Promise;
+
+const mapStateToProps = state => ({
+  appName: state.appName
+});
+
+class Home extends React.Component {
+  componentWillMount() {
+    this.props.onLoad(agent.Articles.all());
+  }
+
+  render() {
+    return (
+      <div className="home-page">
+        <Banner appName={this.props.appName} />
+
+        <div className="container page">
+          <div className="row">
+            <MainView />
+
+            <div className="col-md-3">
+              <div className="sidebar">
+                <p>Popular Tags</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
+import App from './App';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { applyMiddleware, createStore } from 'redux';
+import {Provider} from 'react-redux';
+import { promiseMiddleware } from './middleware';
+import { bindActionCreators } from 'redux';
 
-class App extends React.Component {
-  render() {
-    return (
-      <h1>Hello, World!</h1>
-    );
+const defaultState = {
+  appName: 'conduit',
+  articles: null
+};
+
+const reducer = function(state = defaultState, action) {
+  switch (action.type) {
+    case 'HOME_PAGE_LOADED':
+      return {...state, articles: action.payload.articles};
   }
-}
+  return state;
+};
+const store = createStore(reducer, applyMiddleware(promiseMiddleware));
 
 ReactDOM.render((
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
+  
 ), document.getElementById('root'));
